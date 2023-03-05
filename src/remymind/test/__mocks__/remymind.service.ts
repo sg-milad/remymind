f08032030f1d4435eb751a5e3cde133b5dbe846c
milad <miladbachari14@gmail.com>
2023-03-05 11:56:01 +0330

    delete test

--- src/remymind/test/__mocks__/remymind.service.ts
+++
@@ -1,9 +0,0 @@
-import { remymindStub } from "../stubs/remymind.stubs";
-
-export const RemymindService =jest.fn().mockReturnValue({
-    createReminder: jest.fn().mockResolvedValue(remymindStub),
-    getAllReminder:jest.fn().mockResolvedValue(remymindStub),
-    getReminder: jest.fn().mockResolvedValue(remymindStub),
-    updateReminder:jest.fn().mockResolvedValue(remymindStub),
-    deleteReminder:jest.fn().mockResolvedValue(remymindStub),
-})
